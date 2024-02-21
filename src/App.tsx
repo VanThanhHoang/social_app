ab7c1aad9b1722de1e4e10c5807ac2cc43277ef3
phap010620 <117804317+phap010620@users.noreply.github.com>
2024-02-21 21:12:18 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import store, {persistor, useAppSelector} from "@/redux/store";
 import {PersistGate} from "redux-persist/integration/react";
 import AppNavigator from "@/navigation";
 const App = () => {
+    //tesst
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
