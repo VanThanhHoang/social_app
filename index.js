05fee8fc6501a1e519beb7fc0516a5b9c79953c6
nhaattrieeu <work.nhattrieu@gmail.com>
2024-02-22 10:21:15 +0700

    change env config

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/App';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import {FIREBASE_WEB_CLIENT_ID} from "@env"
+console.log('env',{
+    env:process.env.FIREBASE_WEB_CLIENT_ID
+})
 GoogleSignin.configure({
-    webClientId:FIREBASE_WEB_CLIENT_ID
+    webClientId:process.env.FIREBASE_WEB_CLIENT_ID || ''
 });
 AppRegistry.registerComponent(appName, () => App);
