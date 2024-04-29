e67cbaec857a8eeb8756d44a3e5447b20d769fc8
rdJS <127029284+VanThanhHoang@users.noreply.github.com>
2024-04-30 01:09:13 +0700

    feat

--- src/network/axiosInstance.ts
+++ src/network/axiosInstance.ts
@@ -3,11 +3,11 @@ import {localStorage} from '@/utils';
 
 const AxiosInstance = (contentType = 'application/json') => {
 
-  //const BASE_URL = 'https://sever-social-media-app.onrender.com/';
-  const BASE_URL = 'http://192.168.1.3:8000/';
+  const BASE_URL = 'https://sever-social-media-app.onrender.com/';
+  // const BASE_URL = 'http://192.168.1.3:8000/';
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000, // có tác dụng khi gọi api lâu quá thì sẽ báo lỗi
+    timeout: 10000, // có tác dụng khi gọi api lâu quá thì sẽ   báo lỗi
   });
 
   axiosInstance.interceptors.request.use(
