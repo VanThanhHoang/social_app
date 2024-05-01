f498e4f084b74a0686c4014d98429aaeae0527c7
rdJS <127029284+VanThanhHoang@users.noreply.github.com>
2024-05-01 21:08:34 +0700

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
-import { Provider } from "react-redux";
-import store, { persistor, useAppSelector } from "@/redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import AppNavigator from "@/navigation";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
+import store, {persistor, useAppSelector} from '@/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import AppNavigator from '@/navigation';
+import messaging from '@react-native-firebase/messaging';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Alert} from 'react-native';
+import CustomToast from './components/Toast/CutomToast';
+import { use } from 'i18next';
 const App = () => {
-    //tesst
-    return (
-        <GestureHandlerRootView style={{flex:1}}>
-            <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <AppNavigator/>
-            </PersistGate>
-        </Provider>
-        </GestureHandlerRootView>
-    );
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      CustomToast({
+        type: 'success',
+        message: remoteMessage.notification?.body ,
+      });
+    });
+    return unsubscribe;
+  }, []);
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigator />
+        </PersistGate>
+      </Provider>
+    </GestureHandlerRootView>
+  );
 };
 export default App;
